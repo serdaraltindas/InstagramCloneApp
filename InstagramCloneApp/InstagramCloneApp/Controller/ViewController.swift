804fdf3f88f4af552842f2e1485d20b0aa7042c1
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-04-25 14:11:09 +0300

    FireBase User

--- InstagramCloneApp/InstagramCloneApp/Controller/ViewController.swift
+++ InstagramCloneApp/InstagramCloneApp/Controller/ViewController.swift
@@ -1,25 +1,43 @@
-
-
 import UIKit
 import Firebase
 import FirebaseAuth
 
 class ViewController: UIViewController {
-
+    //textField
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
     }
-
+    
     @IBAction func girisYapButtonClicked(_ sender: UIButton) {
         performSegue(withIdentifier: "toFeedVC", sender: self)
     }
     
     @IBAction func kayitOlButtonClicked(_ sender: UIButton) {
-        performSegue(withIdentifier: "toFeedVC", sender: self)
+        
+        if emailTextField.text != "" && passwordTextField.text != "" {
+            Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (authdataresult, error) in
+                if error != nil {
+                    self.hataMesajı(title: "Hata!", message: error?.localizedDescription ?? "Hata!")
+                }else{
+                    self.performSegue(withIdentifier: "toFeedVC", sender: self)
+                }
+            }
+        } else {
+            //error!
+            hataMesajı(title: "Hata!", message: "Kullanıcı adı veya parola hatalı, tekrar deneyiniz!")
+        }
+    }
+    //alert message
+    func hataMesajı(title : String , message : String){
+        let alertMessage = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+        alertMessage.addAction(okButton)
+        self.present(alertMessage, animated: true)
     }
 }
 
