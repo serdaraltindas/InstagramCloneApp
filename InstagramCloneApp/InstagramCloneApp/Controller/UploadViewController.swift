9190c5b5fbd4a56e07ae4d5ecb26338c8938f9de
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-04-25 17:26:56 +0300

    imagePicker

--- InstagramCloneApp/InstagramCloneApp/Controller/UploadViewController.swift
+++ InstagramCloneApp/InstagramCloneApp/Controller/UploadViewController.swift
@@ -1,13 +1,7 @@
-//
-//  UploadViewController.swift
-//  InstagramCloneApp
-//
-//  Created by Serdar Altındaş on 25.04.2023.
-//
 
 import UIKit
 
-class UploadViewController: UIViewController {
+class UploadViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var imageView: UIImageView!
@@ -15,10 +9,35 @@ class UploadViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
         
+        //close keyboard.
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(klavyeyiKapat))
+        view.addGestureRecognizer(gestureRecognizer)
+        
+        imageView.isUserInteractionEnabled = true
+        let imageGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(gorselSec))
+        imageView.addGestureRecognizer(imageGestureRecognizer)
     }
+    @objc func gorselSec(){
+        //kütüphane git-> pickerController
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        //galeri-> göster
+        present(picker, animated: true)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        //medya seçimi bitti.
+        imageView.image = info[.editedImage] as? UIImage
+        //açılan galeriyi kapat.
+        self.dismiss(animated: true)
+    }
+    @objc func klavyeyiKapat(){
+        //aksiyonları kapat.
+        view.endEditing(true)
+    }
+    
     @IBAction func uploadButtonClicked(_ sender: UIButton) {
         
     }
