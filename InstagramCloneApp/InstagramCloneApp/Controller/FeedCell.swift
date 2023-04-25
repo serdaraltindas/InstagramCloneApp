2dbfbab5b012438ae255df770416c588367fc175
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-04-26 01:09:26 +0300

    fixed

--- InstagramCloneApp/InstagramCloneApp/Controller/FeedCell.swift
+++ InstagramCloneApp/InstagramCloneApp/Controller/FeedCell.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class FeedCell: UITableViewCell {
-
+    
     @IBOutlet weak var commentText: UILabel!
     @IBOutlet weak var postImageView: UIImageView!
     @IBOutlet weak var emailText: UILabel!
@@ -17,11 +17,11 @@ class FeedCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
 }
