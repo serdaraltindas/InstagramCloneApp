5451909dea8c989d339b20de81a2c979c66dd5f0
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-04-26 00:59:52 +0300

    firebaseverilerial

--- InstagramCloneApp/InstagramCloneApp/Controller/FeedViewController.swift
+++ InstagramCloneApp/InstagramCloneApp/Controller/FeedViewController.swift
@@ -6,28 +6,69 @@
 //
 
 import UIKit
+import Firebase
 
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+    
     @IBOutlet weak var tableView: UITableView!
+    
+    var emailDizisi = [String]()
+    var yorumDizisi = [String]()
+    var gorselDizisi = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
-    
+        
+        firebaseVerileriAl()
     }
+    
+    func firebaseVerileriAl() {
+        let firestoreDatabase = Firestore.firestore()
+        firestoreDatabase.collection("Posted").addSnapshotListener { (snapshot, error) in
+            if error != nil {
+                self.hataMesajı(title: "Hata!", message: error?.localizedDescription ?? "Hata!")
+            }else{
+                if snapshot?.isEmpty != true && snapshot != nil {
+                    for document in snapshot!.documents {
+                        //let documentId = document.documentID
+                        
+                        if let gorselUrl = document.get("gorselurl") as? String {
+                            self.gorselDizisi.append(gorselUrl)
+                        }
+                        if let yorum = document.get("yorum") as? String {
+                            self.yorumDizisi.append(yorum)
+                        }
+                        if let email = document.get("email") as? String {
+                            self.emailDizisi.append(email)
+                        }
+                        
+                    }
+                    self.tableView.reloadData()
+                }
+            }
+            
+        }
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return emailDizisi.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! FeedCell
-        cell.emailText.text = "test"
-        cell.commentText.text = "test"
+        cell.emailText.text = emailDizisi[indexPath.row]
+        cell.commentText.text = yorumDizisi[indexPath.row]
         cell.postImageView.image = UIImage()
         return cell
         
+    }
+    func hataMesajı(title : String , message : String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+        alert.addAction(okButton)
+        present(alert, animated: true)
     }
 }
