f0e5ddd3a611cadcf1b11e00515c52588997544e
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-04-25 13:18:31 +0300

    settings vc

--- InstagramCloneApp/InstagramCloneApp/Controller/ViewController.swift
+++ InstagramCloneApp/InstagramCloneApp/Controller/ViewController.swift
@@ -6,11 +6,20 @@ import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
     }
 
+    @IBAction func girisYapButtonClicked(_ sender: UIButton) {
+        performSegue(withIdentifier: "toFeedVC", sender: self)
+    }
+    
+    @IBAction func kayitOlButtonClicked(_ sender: UIButton) {
+        performSegue(withIdentifier: "toFeedVC", sender: self)
+    }
 }
 
